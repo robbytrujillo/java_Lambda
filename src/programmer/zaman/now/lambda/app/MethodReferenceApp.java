21f8362e97e7f5dcee09fb4203fdb7e9173b322c
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-05 10:10:17 +0700

    Update Method Reference

--- src/programmer/zaman/now/lambda/app/MethodReferenceApp.java
+++ src/programmer/zaman/now/lambda/app/MethodReferenceApp.java
@@ -2,15 +2,50 @@ package programmer.zaman.now.lambda.app;
 
 import programmer.zaman.now.lambda.util.StringUtil;
 
+import java.util.Locale;
+import java.util.function.Function;
 import java.util.function.Predicate;
-
+    //Method Reference Static
 public class MethodReferenceApp {
     public static void main(String[] args) {
 
         //Predicate<String> predicateIsLowerCase = value -> StringUtil.isLowerCase(value);
-        Predicate<String> predicateIsLowerCase = StringUtil :: isLowerCase;
+        Predicate<String> predicateIsLowerCase = StringUtil::isLowerCase;
 
+        System.out.println(predicateIsLowerCase.test("robby"));
         System.out.println(predicateIsLowerCase.test("Robby"));
+
+        //Method Reference di Parameter
+        //Function<String, String> functionUpper = value -> value.toUpperCase();
+        Function<String, String> functionUpper = String::toUpperCase;
+
+        System.out.println(functionUpper.apply("Robby"));
+    }
+
+    //Method Reference Bukan Static
+    public void run() {
+        //MethodReferenceApp app = new MethodReferenceApp();
+
+        Predicate<String> predicateIsLowerCase = this::isLowerCase;
+
+        System.out.println(predicateIsLowerCase.test("robby"));
         System.out.println(predicateIsLowerCase.test("Robby"));
+    }
+        //Method reference yang milik orang lain
+        public void run2() {
+            MethodReferenceApp app = new MethodReferenceApp();
+            Predicate<String> predicateIsLowerCase = app::isLowerCase;
+
+            System.out.println(predicateIsLowerCase.test("robby"));
+            System.out.println(predicateIsLowerCase.test("Robby"));
+        }
+
+    public boolean isLowerCase(String value) {
+        for(var c : value.toCharArray()) {
+            if (!Character.isLowerCase(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
