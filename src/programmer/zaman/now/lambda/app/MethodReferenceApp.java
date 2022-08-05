f812504c5f5cea7cef73c6eb0db6c94df7e2971a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-05 09:45:12 +0700

    Up Method Reference

--- src/programmer/zaman/now/lambda/app/MethodReferenceApp.java
+++ src/programmer/zaman/now/lambda/app/MethodReferenceApp.java
@@ -1,9 +1,15 @@
 package programmer.zaman.now.lambda.app;
 
+import programmer.zaman.now.lambda.util.StringUtil;
+
+import java.util.function.Predicate;
+
 public class MethodReferenceApp {
     public static void main(String[] args) {
 
+        Predicate<String> predicateIsLowerCase = value -> StringUtil.isLowerCase(value);
 
-
+        System.out.println(predicateIsLowerCase.test("Robby"));
+        System.out.println(predicateIsLowerCase.test("Robby"));
     }
 }
