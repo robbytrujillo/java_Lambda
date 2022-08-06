92891b9179a87cf6964fb85ebef0cfa3f4c7cbf9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-06 19:28:22 +0700

    Lamda di Collection

---
+++ src/programmer/zaman/now/lambda/app/ForEachApp.java
@@ -0,0 +1,11 @@
+package programmer.zaman.now.lambda.app;
+
+import java.util.List;
+
+public class ForEachApp {
+    public static void main(String[] args) {
+
+        List<String> list = List.of("Robby", :"Ilham", "Kusuma");
+
+    }
+}
