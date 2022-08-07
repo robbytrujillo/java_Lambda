402a8e191289cfc6e9e359a7475d2611388e1816
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-07 07:11:29 +0700

    Update Lambda di Collection

--- src/programmer/zaman/now/lambda/app/ForEachApp.java
+++ src/programmer/zaman/now/lambda/app/ForEachApp.java
@@ -1,11 +1,33 @@
 package programmer.zaman.now.lambda.app;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ForEachApp {
     public static void main(String[] args) {
 
-        List<String> list = List.of("Robby", :"Ilham", "Kusuma");
+        List<String> list = List.of("Robby", "Ilham", "Kusuma");
 
+        //for loop
+        for (var value : list) {
+            System.out.println(value);
+        }
+
+        //forEach anonimous class
+        list.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String value) {
+                System.out.println(value);
+            }
+        });
+        
+        //lambda
+        //Consumer<String> lambda = value -> System.out.println(value);
+        list.forEach(value -> System.out.println(value));
+
+        //lambda method reference
+        list.forEach(System.out::println);
+            
+        }
     }
-}
+
