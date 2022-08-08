14566e0a888f8698250f040e13f18c5d5a715865
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-09 04:11:08 +0700

    Lambda Lazy Parameter

---
+++ src/programmer/zaman/now/lambda/app/LazyApp.java
@@ -0,0 +1,16 @@
+package programmer.zaman.now.lambda.app;
+
+public class LazyApp {
+    public static void main(String[] args) {
+
+    testScore(60, "Robby");
+    }
+
+    public static void testScore(int score, String name) {
+        if(score > 80) {
+            System.out.println("Selamat " + name + ", Anda Lulus");
+        }else {
+            System.out.println("Coba Lagi Tahun Depan");
+        }
+    }
+}
