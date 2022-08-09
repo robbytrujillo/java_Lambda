d3c32a6f91600177bf2bb34949b5a17256edab9e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-10 06:35:12 +0700

    Update Lambda Lazy Parameter

--- src/programmer/zaman/now/lambda/app/LazyApp.java
+++ src/programmer/zaman/now/lambda/app/LazyApp.java
@@ -1,21 +1,35 @@
 package programmer.zaman.now.lambda.app;
 
+import java.util.function.Supplier;
+
 public class LazyApp {
     public static void main(String[] args) {
 
-    testScore(60, "Robby");
+    //testScore(60, "Robby");
+    //testScore(60, getName());
+
+    //lambda
+    testScore(60, () -> getName());
     }
 
-    public static void testScore(int score, String name) {
+//    public static void testScore(int score, String name) {
+//        if(score > 80) {
+//            System.out.println("Selamat " + name + ", Anda Lulus");
+//        }else {
+//            System.out.println("Coba Lagi Tahun Depan");
+//        }
+//    }
+
+    public static void testScore(int score, Supplier<String> name) {
         if(score > 80) {
-            System.out.println("Selamat " + name + ", Anda Lulus");
+            System.out.println("Selamat " + name.get() + ", Anda Lulus");
         }else {
             System.out.println("Coba Lagi Tahun Depan");
         }
     }
 
     public static String getName(){
-        System.out.println("Method getName dipanggil");
+        System.out.println("Method getName() dipanggil");
         return "Robby";
     }
 }
