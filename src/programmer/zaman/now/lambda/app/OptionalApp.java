c352f352ab412096b94da69863ee92e663d5f793
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-11 20:05:36 +0700

    Lambda di Optional

---
+++ src/programmer/zaman/now/lambda/app/OptionalApp.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.lambda.app;
+
+public class OptionalApp {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static void sayHello(String name) {
+        String nameUpper = name.toUpperCase();
+    }
+}
