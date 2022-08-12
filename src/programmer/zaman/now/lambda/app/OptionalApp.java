1d02c4c1d0836dc76c5ed9dd085c88347311d7db
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-12 07:09:38 +0700

    Update Lambda di Optional

--- src/programmer/zaman/now/lambda/app/OptionalApp.java
+++ src/programmer/zaman/now/lambda/app/OptionalApp.java
@@ -1,13 +1,29 @@
 package programmer.zaman.now.lambda.app;
 
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class OptionalApp {
     public static void main(String[] args) {
 
+    sayHello("Robby");
 
+//    Bakal error
+//    String name = null;
+//    sayHello(name);
 
     }
 
     public static void sayHello(String name) {
-        String nameUpper = name.toUpperCase();
+
+        Optional<String> optionalName = Optional.ofNullable(name);
+
+        Optional<String> optionalNameUpper = optionalName.map(value -> value.toUpperCase());
+
+        optionalNameUpper.ifPresent(value -> System.out.println("Hello " + value));
+
+//        String nameUpper = name.toUpperCase();
+//        System.out.println("Hello " + nameUpper);
     }
 }
