64e87353502831323da4855a064bb095d1cbc2b9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-14 23:47:54 +0700

    Lambda di Optional Method Reference

--- src/programmer/zaman/now/lambda/app/OptionalApp.java
+++ src/programmer/zaman/now/lambda/app/OptionalApp.java
@@ -1,5 +1,6 @@
 package programmer.zaman.now.lambda.app;
 
+import java.util.Locale;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -17,16 +18,22 @@ public class OptionalApp {
 
     public static void sayHello(String name) {
 
+        Optional.ofNullable(name)
+                .map(String::toUpperCase)
+                .ifPresent(System.out::println);
+
 //        Optional<String> optionalName = Optional.ofNullable(name);
 //
 //        Optional<String> optionalNameUpper = optionalName.map(value -> value.toUpperCase());
 //
 //        optionalNameUpper.ifPresent(value -> System.out.println("Hello " + value));
 
-        if (name != null) {
-            String upperName = name.toUpperCase();
-            System.out.println("Hello " + upperName);
-        }
+          //if checkNull
+
+//        if (name != null) {
+//            String upperName = name.toUpperCase();
+//            System.out.println("Hello " + upperName);
+//        }
 
 //        String nameUpper = name.toUpperCase();
 //        System.out.println("Hello " + nameUpper);
